3ad0e65ab4ebedf14707552eec7a44fba54ff586
Raffaele Grieco <62102593+Baraff24@users.noreply.github.com>
2024-12-16 17:05:13 +0100

    bug fixing

--- app/config/settings/development.py
+++ app/config/settings/development.py
@@ -2,9 +2,9 @@ from .base import *
 
 # Media and static files
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../core/', 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../vol/', 'mediafiles')
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../../core/', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../vol/', 'staticfiles')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
